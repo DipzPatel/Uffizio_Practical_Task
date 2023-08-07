b6f7357f70bbf8a4c029b47b1bd1ed0ea33a2081
Deep <deepmt23>
2023-08-07 11:49:50 +0530

    Initial Commit

---
+++ app/src/main/java/my/agro/uffizio_practical_task/MainActivity.kt
@@ -0,0 +1,11 @@
+package my.agro.uffizio_practical_task
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
